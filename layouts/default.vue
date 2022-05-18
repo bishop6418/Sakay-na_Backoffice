609110f4dd8c53d8f0738e7561846b2e65b7d837
Olivier <olivier@studioseizh.com>
2022-05-18 15:37:07 +0800

    Added Verify Users

--- layouts/default.vue
+++ layouts/default.vue
@@ -109,6 +109,11 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
+          title: 'Unverified Users',
+          to: '/unverified'
+        },
+        {
+          icon: 'mdi-chart-bubble',
           title: 'Histories',
           to: '/histories'
         },
